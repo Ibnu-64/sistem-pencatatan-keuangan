bb3f2c0cb382a4a056839db84431093a9d8df50c
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-05-21 12:24:19 +0800

    refactor: display data

--- app.py
+++ app.py
@@ -13,66 +13,59 @@ locale.setlocale(locale.LC_TIME, 'id_ID.UTF-8')
 
 @app.route('/')
 def index():
-    transactions_by_date = {
-        "2025-05-18": [
-            {
-                "type": "expense",
-                "category": "Makanan",
-                "amount": 55000,
-                "item": datetime(2025, 5, 18, 20, 15),
-                "icon": "fa-solid fa-burger",
-            },
-            {
-                "type": "income",
-                "category": "Tunai",
-                "amount": 20000,
-                "item": datetime(2025, 5, 18, 14, 35),
-                "icon": "fa-solid fa-wallet",
-            },
-        ],
-        "2025-05-17": [
-            {
-                "type": "expense",
-                "category": "Minum",
-                "amount": 10000,
-                "item": datetime(2025, 5, 17, 1, 15),
-                "icon": "fa-solid fa-mug-hot",
-            }
-        ],
-        "2025-05-16": [
-            {
-                "type": "expense",
-                "category": "Makan",
-                "amount": 15000,
-                "item": datetime(2025, 5, 16, 1, 15),
-                "icon": "fa-solid fa-burger",
-            },
-            {
-                "type": "income",
-                "category": "Tunai",
-                "amount": 3245,
-                "item": datetime(2025, 5, 16, 20, 55),
-                "icon": "fa-solid fa-wallet",
-                
-            },
-            {
-                "type": "income",
-                "category": "Tunai",
-                "amount": 200000,
-                "item": datetime(2025, 5, 16, 10, 12),
-                "icon": "fa-solid fa-wallet",
-                
-            },
-            {
-                "type": "expense",
-                "category": "Minum",
-                "amount": 10000,
-                "item": datetime(2025, 5, 16, 7, 40),
-                "icon": "fa-solid fa-mug-hot",
-                
-            }
-        ],
-    }
+    transactions_by_date = [
+        {
+            "date": '2025-05-18',
+            "type": "expense",
+            "category": "Makanan",
+            "amount": 55000,
+        },
+        {
+            "date": '2025-05-18',
+            "type": "income",
+            "category": "Tunai",
+            "note": "Gaji",
+            "amount": 20000,
+        },
+        {
+            "date": '2025-05-17',
+            "type": "expense",
+            "category": "Minum",
+            "note": "Teh",
+            "amount": 10000,
+        },
+        {
+            "date": '2025-05-16',
+            "type": "expense",
+            "category": "Makan",
+            "note": "Nasi Goreng",
+            "amount": 15000,
+        },
+        {
+            "date": '2025-05-16',
+            "type": "income",
+            "category": "Tunai",
+            "note": "-",
+            "amount": 3245,
+            
+        },
+        {
+            "date": '2025-05-16',
+            "type": "income",
+            "category": "Tunai",
+            "note": "-",
+            "amount": 200000,
+            
+        },
+        {
+            "date": '2025-05-16',
+            "type": "expense",
+            "category": "Minum",
+            "note": "-",
+            "amount": 10000,
+            
+        }
+    ]
     return render_template("index.html", transactions_by_date=transactions_by_date)
 
 BULAN_INDONESIA = [
@@ -91,8 +84,14 @@ def inject_functions():
         if isinstance(date_input, str):
             date_input = datetime.strptime(date_input, "%Y-%m-%d")
         return date_input.strftime("%A")
+    
+    # def format_currency(value):
+    #     return f"{value:,.0f}".replace(",", ".")
+    
+    def format_currency(value):
+        return f"Rp {value:,.2f}"
 
-    return dict(format_tanggal=format_tanggal, get_day_label=get_day_label)
+    return dict(format_tanggal=format_tanggal, get_day_label=get_day_label, format_currency=format_currency)
 
 @app.route('/api/data') # Endpoint API /api/data
 def get_data(): # Fungsi untuk mengembalikan data JSON
