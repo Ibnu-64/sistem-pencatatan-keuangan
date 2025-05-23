afb5aa8aa92ac1299109b9b3017ce9812ac16415
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-05-23 19:43:10 +0800

    refactor: routes index

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
+# Import routes
+from .routes.index import index_bp
+
 def create_app():
+    """Buat aplikasi Flask"""
     app = Flask(__name__)
+    app.register_blueprint(index_bp, url_prefix='/')
     return app
