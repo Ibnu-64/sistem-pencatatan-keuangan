0d727c5bb67ccfb43612627382ea13b86475ee6b
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-05-25 19:43:13 +0800

    add: trigger

---
+++ app/sql/trigger.sql
@@ -0,0 +1,83 @@
+USE sistem_pencatatan_keuangan;
+
+-- Perbaharui data financial_summary saat ada transaksi baru
+DELIMITER $$
+CREATE TRIGGER trg_add
+AFTER INSERT ON transactions
+FOR EACH ROW
+BEGIN
+    IF NEW.type_id = 'income' THEN
+        UPDATE financial_summary
+        SET total_income = total_income + NEW.amount
+        WHERE id = 1;
+    ELSE
+        UPDATE financial_summary
+        SET total_expense = total_expense + NEW.amount
+        WHERE id = 1;
+    END IF;
+END $$
+DELIMITER ;
+
+-- Perbaharui data financial_summary saat ada transaksi di hapus
+DELIMITER $$
+CREATE TRIGGER trg_delete
+AFTER DELETE ON transactions
+FOR EACH ROW
+BEGIN
+    IF OLD.type_id = 'income' THEN
+        UPDATE financial_summary
+        SET total_income = total_income - OLD.amount
+        WHERE id = 1;
+    ELSE
+        UPDATE financial_summary
+        SET total_expense = total_expense - OLD.amount
+        WHERE id = 1;
+    END IF;
+END $$
+DELIMITER ;
+
+-- Perbaharui data financial_summary saat ada transaksi di update
+DELIMITER $$
+CREATE TRIGGER trg_update
+AFTER UPDATE ON transactions
+FOR EACH ROW
+BEGIN
+    -- Jika tipe tidak berubah
+    IF NEW.type_id = OLD.type_id THEN
+        IF NEW.type_id = 'income' THEN
+            UPDATE financial_summary
+            SET total_income = total_income + NEW.amount - OLD.amount
+            WHERE id = 1;
+        ELSE
+            UPDATE financial_summary
+            SET total_expense = total_expense + NEW.amount - OLD.amount
+            WHERE id = 1;
+        END IF;
+    
+    -- Jika tipe berubah
+    ELSE
+        -- Kurangi dari tipe lama
+        IF OLD.type_id = 'income' THEN
+            UPDATE financial_summary
+            SET total_income = total_income - OLD.amount
+            WHERE id = 1;
+        ELSE
+            UPDATE financial_summary
+            SET total_expense = total_expense - OLD.amount
+            WHERE id = 1;
+        END IF;
+        
+        -- Tambah ke tipe baru
+        IF NEW.type_id = 'income' THEN
+            UPDATE financial_summary
+            SET total_income = total_income + NEW.amount
+            WHERE id = 1;
+        ELSE
+            UPDATE financial_summary
+            SET total_expense = total_expense + NEW.amount
+            WHERE id = 1;
+        END IF;
+    END IF;
+END $$
+DELIMITER ;
+
